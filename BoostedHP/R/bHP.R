2285db287482f39489078ba33370f587e9e5c749
Zhentao <shizhentao@gmail.com>
2020-08-10 22:40:18 +0800

    rename the package as bHP

---
+++ BoostedHP/R/bHP.R
@@ -0,0 +1,11 @@
+#' BoostedHP: A package for the boosted HP filter
+#'
+#' Iterated HP filtering by Phillips and Shi (2020)
+#'
+#' @section Boosted HP main function:
+#'
+#' BoostedHP is the main function
+#'
+#' @docType package
+#' @name bHP
+NULL
